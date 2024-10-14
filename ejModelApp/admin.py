2128d47e7c6c3908351f2e517f17768b47ef58c8
Cristofher <Cristofher@gmail.com>
2024-10-14 11:11:25 -0300

    Clase CRUD

--- ejModelApp/admin.py
+++ ejModelApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+""" Se agrega el modelo de Empleados """
+from ejModelApp.models import Empleado
 
 # Register your models here.
+
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ('nombre','apellido','correo','telefono','edad')
+
+admin.site.register(Empleado, EmpleadoAdmin)
